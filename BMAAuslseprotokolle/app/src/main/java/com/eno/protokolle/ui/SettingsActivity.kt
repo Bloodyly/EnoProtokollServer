89fcabfd483a0d6518bc6a1cfb12718d3db49a55
Bloodyly <155741658+Bloodyly@users.noreply.github.com>
2025-09-09 07:48:29 +0200

    Update SettingsActivity.kt

--- BMAAuslseprotokolle/app/src/main/java/com/eno/protokolle/ui/SettingsActivity.kt
+++ BMAAuslseprotokolle/app/src/main/java/com/eno/protokolle/ui/SettingsActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.InetSocketAddress
 import java.net.Socket
+import com.eno.protokolle.R
 
 
 class SettingsActivity : ComponentActivity() {
